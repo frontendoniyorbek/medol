2ca62686d03016809ba52e40f46115765e42e2d3
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2023-06-21 13:21:51 +0500

    Navbar component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,31 @@
+import Link from 'next/link';
+import navbar from '../styles/styles.module.css';
+
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<nav className='opacity-90 bg-white rounded-[35px] mb-[71px] pt-6 pr-8 pb-6 pl-12'>
+			<ul className='flex items-center justify-center list-none'>
+				<li className={navbar.navLi}>
+					<Link href={'#'}>МАГАЗИН</Link>
+				</li>
+				<li className={navbar.navLi}>
+					<Link href={'#'}>О КОМПАНИИ</Link>
+				</li>
+				<li className={navbar.navLi}>
+					<Link href={'#'}>ПРОДУКЦИЯ</Link>
+				</li>
+				<li className={navbar.navLi}>
+					<Link href={'#'}>УСЛУГИ</Link>
+				</li>
+				<li className={navbar.navLi}>
+					<Link href={'#'}>АКЦИИ И НОВОСТИ</Link>
+				</li>
+				<li className={navbar.navLi}>
+					<Link href={'#'}>ОБРАТНАЯ СВЯЗЬ</Link>
+				</li>
+			</ul>
+		</nav>
+	);
 };
 
 export default Navbar;
