6b026d549127cc42bcbf3b5ec975bb7b3db35dec
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2023-06-23 07:16:21 +0500

    motion

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,17 @@
 import '@/styles/globals.css';
 import 'react-multi-carousel/lib/styles.css';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<AnimatePresence>
+			<motion.div
+				initial={{ opacity: 0, y: 15 }}
+				animate={{ opacity: 1, y: 0 }}
+				exit={{ opacity: 0, y: 15 }}
+				transition={{ delay: 0.5 }}>
+				<Component {...pageProps} />
+			</motion.div>
+		</AnimatePresence>
+	);
 }
